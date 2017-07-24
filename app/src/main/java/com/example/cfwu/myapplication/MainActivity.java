b8a4927523b8caefa01387569ea6141811062092
¨cf-wu¨ <chufei_working@126.com>
2017-07-25 00:57:17 +0800

    ¨first_commit¨

---
+++ app/src/main/java/com/example/cfwu/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.cfwu.myapplication;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button mButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton=(Button) findViewById(R.id.click);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MediaPlayer mediaPlayer=new MediaPlayer();
+//                mediaPlayer.setDataSource(this,);
+            }
+        });
+    }
+}
